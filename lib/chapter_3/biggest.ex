7a5a5b9992edb0f9b4a76091dd40fac25409e933
Paul Wilson <paulanthonywilson@gmail.com>
2018-06-15 17:26:30 +0100

    Rewrite biggest with similar implementations to the book.

--- lib/chapter_3/biggest.ex
+++ lib/chapter_3/biggest.ex
@@ -1,10 +1,11 @@
 defmodule Biggest do
   def biggest([]), do: nil
 
-  def biggest(list) do
-    Enum.reduce(list, fn
-      lhs, rhs when lhs > rhs -> lhs
-      _lhs, rhs -> rhs
-    end)
+  def biggest([h | t]) do
+    biggest(t, h)
   end
+
+  def biggest([], biggest), do: biggest
+  def biggest([h | t], biggest) when h > biggest, do: biggest(t, h)
+  def biggest([_ | t], biggest), do: biggest(t, biggest)
 end
