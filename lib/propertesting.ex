41a4705aa139d7bc5540a9e738fa506e73ba4d1e
Paul Wilson <paulanthonywilson@gmail.com>
2018-06-15 17:56:27 +0100

    flatten the directories

--- lib/propertesting.ex
+++ lib/propertesting.ex
@@ -1,18 +1,2 @@
 defmodule Propertesting do
-  @moduledoc """
-  Documentation for Propertesting.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Propertesting.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/propertesting_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule PropertestingTest do
-  use ExUnit.Case
-  doctest Propertesting
-
-  test "greets the world" do
-    assert Propertesting.hello() == :world
-  end
-end
