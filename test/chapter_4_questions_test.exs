a387ac39fcf52a6ae8bf6301908f45fc008d1db8
Paul Wilson <paulanthonywilson@gmail.com>
2018-06-19 15:22:24 +0100

    prop_sort() test - Ch 4, Q4.

--- test/chapter_4_questions_test.exs
+++ test/chapter_4_questions_test.exs
@@ -2,7 +2,7 @@ defmodule Chapter4QuestionsTest do
   use ExUnit.Case
   use ExUnitProperties
 
-  property "lists:seq produces a list of a range between a number and a larger or equal number" do
+  property ":lists.seq/2 produces a list of a range between a number and a larger or equal number" do
     check all start <- integer(),
               finish <- integer() do
       case finish - start do
@@ -25,4 +25,33 @@ defmodule Chapter4QuestionsTest do
   defp increments(_, []), do: true
   defp increments(n, [h | t]) when h == n + 1, do: increments(h, t)
   defp increments(_, _), do: false
+
+  property ":lists.keysort/2 sorts tuples" do
+    tuple_gen = tuple({integer(), string(:alphanumeric)})
+
+    check all tuple_list <- list_of(tuple_gen),
+              order_by_element <- integer(1..2) do
+      sorted = :lists.keysort(order_by_element, tuple_list)
+
+      assert ordered_by_key(order_by_element, sorted)
+
+      assert length(tuple_list) == length(sorted)
+    end
+  end
+
+  defp ordered_by_key(_n, []), do: true
+  defp ordered_by_key(n, [h | t]), do: ordered_by_key(n, h, t)
+  defp ordered_by_key(_n, _last, []), do: true
+
+  defp ordered_by_key(n, last, [h | t]) do
+    # elem is zero indexed, unlike :lists.keysort/2
+    last_key = elem(last, n - 1)
+    key = elem(h, n - 1)
+
+    if last_key > key do
+      false
+    else
+      ordered_by_key(n, h, t)
+    end
+  end
 end
